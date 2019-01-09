8ad3c6a596f7a72617de19b79503644a0581432e
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2019-01-08 21:40:16 -0500

    added stacks

---
+++ stacks-and-queues/stacks-arrays.js
@@ -0,0 +1,23 @@
+class Stack {
+  constructor(){
+    this.array = []
+  }
+  peek() {
+    return this.array[this.array.length - 1]
+  }
+  push(value) {
+    this.array.push(value)
+    return this
+  }
+  pop() {
+    this.array.pop()
+    return this;
+  }
+}
+
+const myStack = new Stack();
+myStack.push('google');
+myStack.push('udemy');
+myStack.push('discord');
+myStack.pop();
+myStack.peek()
